5387525402cc45376482fa65c17129fa0f11c0f4
Cixsed <asisrodsan@gmail.com>
2020-01-27 14:08:32 -0600

    Funciones básicas:6

--- msg/messenger.js
+++ msg/messenger.js
@@ -18,7 +18,7 @@ app.post('/webhook', (req,res) => {
             // will only ever contain one message, so we get index 0
             
             let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+            console.log(webhook_event.message);
         });
         
         if (webhook_event.message) {
