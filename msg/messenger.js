b9fd8e80d51eb3701520ee2f557bc293d5d7c967
Cixsed <asisrodsan@gmail.com>
2020-01-27 15:23:59 -0600

    Primeras Respuestas:1

--- msg/messenger.js
+++ msg/messenger.js
@@ -2,7 +2,8 @@
 
 const 
 express = require('express'),
-app = express();
+app = express(),
+request = require('request');
 
 // Get request for base url
 app.post('/webhook', (req,res) => {
@@ -10,31 +11,35 @@ app.post('/webhook', (req,res) => {
     
     // Checks if this is an event from a page subscription
     if (body.object === 'page') {
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
         
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
             
             // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            
+            // will only ever contain one message, so we get index 0            
             let webhook_event = entry.messaging[0];
             console.log(webhook_event.message);
 
-            if (webhook_event.message) {
-                handleMessage(res);        
-            } else {
-                res.sendStatus(404);
-            }
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
 
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);        
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+  }
+            
         });
-        
-        
-        // Returns a '200 OK' response to all requests
-        //     res.status(200).send('EVENT_RECEIVED');
-        // } else {
-            //     // Returns a '404 Not Found' if event is not from a page subscription
-            //     res.sendStatus(404);
-        }
+                
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
     });
     
     // Adds support for GET requests to our webhook
@@ -64,8 +69,48 @@ app.post('/webhook', (req,res) => {
         
     });
     
-    function handleMessage(res) {
-        res.status(200).send('EXITO');
+    // Handles messages events
+    function handleMessage(sender_psid, received_message) {
+        let response;
+
+        // Check if the message contains text
+        if (received_message.text) {
+            
+            // Create the payload for a basic text message
+            response = {
+                "text": `Mensaje ${received_message} recibido`
+            }
+        }
+
+        // Sends the response message
+        callSendAPI(sender_psid, response);
     }
+
+    // Sends response messages via the Send API
+    function callSendAPI(sender_psid, response) {
+        // Construct the message body
+        let request_body = {
+            "recipient":  {
+                "id": sender_psid
+            },
+            "message": response
+        }
+
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": "https://graph.facebook.com/v2.6/me/messages",
+            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+            "method": "POST",
+            "json": request_body
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('Mensaje enviado');
+            } else {
+                console.error('No se pudo enviar el mensaje:' + err);
+            }
+        });
+    }
+
+
     
     module.exports = app;
