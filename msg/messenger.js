cb9c60e085e76b4ec863c0e3136482e9a3f1a553
Cixsed <asisrodsan@gmail.com>
2020-02-06 14:08:39 -0600

    Pagina o messenger:1

--- msg/messenger.js
+++ msg/messenger.js
@@ -73,11 +73,6 @@ app.post('/webhook', (req, res) => {
                         i18n.setLocale(user.locale);
                         console.log('Nuevo Perfil PSID:', senderPsid, 'con locale:', i18n.getLocale());
                         let referral;
-                        if (webhookEvent.hasOwnProperty('postback') && webhookEvent.postback.hasOwnProperty('referral')) {
-                            referral = true;
-                        } else {
-                            referral = false;
-                        }
                         let receivedMessage = new Receive(users[senderPsid], webhookEvent, referral);
                         return receivedMessage.handleMessage();
                     });
@@ -85,11 +80,6 @@ app.post('/webhook', (req, res) => {
                 i18n.setLocale(users[senderPsid]. locale);
                 console.log('El perfil ya existe, PSID:', senderPsid, 'con locale:', i18n.getLocale());
                 let referral;
-                if (webhookEvent.hasOwnProperty('postback') && webhookEvent.postback.hasOwnProperty('referral')) {
-                    referral = true;
-                } else {
-                    referral = false;
-                }
                 let receivedMessage = new Receive(users[senderPsid], webhookEvent, referral);
                 return receivedMessage.handleMessage();
             }            
