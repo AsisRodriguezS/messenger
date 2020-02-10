47c3c010369c5a096a0ce39fe9aee70e009c7263
Cixsed <asisrodsan@gmail.com>
2020-02-10 17:09:25 -0600

    Imagenes:5

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,8 @@ const
 
     router.route('/').get((req, res) => res.render('pages/home.handlebars'));
     router.route('/images').get((req, res) => res.render('pages/images.handlebars'));
-    router.route('/images/servicio').get((req, res) => res.render('pages/images/servicio.handlebars'));
+    router.route('/images/servicio').get((req, res) => res.render('pages/servicio.handlebars'));
+    router.route('/images/emprendimiento').get((req, res) => res.render('pages/emprendimiento.handlebars'));
 
     router.use(require('../msg/messenger'));
 
