77eb3576135a9b883a578b5c0361d8846734acdc
Cixsed <asisrodsan@gmail.com>
2020-02-10 18:08:59 -0600

    Imagenes:13

--- routes/routes.js
+++ routes/routes.js
@@ -8,9 +8,6 @@ const
     // router.use(json({verify: verifyRequestSignature}));
 
     router.route('/').get((req, res) => res.render('pages/home.handlebars'));
-    router.route('/images').get((req, res) => res.render('pages/images.handlebars'));
-    router.route('/images/servicio').get((req, res) => res.render('pages/servicio.handlebars'));
-    router.route('/images/emprendimiento').get((req, res) => res.render('pages/emprendimiento.handlebars'));
 
     router.use(require('../msg/messenger'));
 
