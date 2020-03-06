716f782c842afef3fac053fc1e1266f0cc9c0c96
Cixsed <asisrodsan@gmail.com>
2020-03-05 19:29:21 -0600

    Mongo

--- msg/messenger.js
+++ msg/messenger.js
@@ -1,21 +1,23 @@
 'use strict';
 
-const 
-express = require('express'),
-request = require('request'),
-Receive = require('./services/receive'),
-GraphAPI = require('./services/graph-api'),
-config = require('./services/config'),
-User = require('./services/user'),
-i18n = require('./idiomas/i18n.config'),
-app = express();
+const
+    express = require('express'),
+    request = require('request'),
+    Receive = require('./services/receive'),
+    GraphAPI = require('./services/graph-api'),
+    config = require('./services/config'),
+    User = require('./services/user'),
+    i18n = require('./idiomas/i18n.config'),
+    mongoClient = require('mongodb').MongoClient,
+    app = express();
 // request = require('request'),
 // VERIFY_TOKEN = process.env.VERIFY_TOKEN,
 // PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
+let urlDB = 'mongodb://localhost:27017';
 let users = {};
 
-app.use(express.json({ verify: verifyRequestSignature}));
+app.use(express.json({ verify: verifyRequestSignature }));
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
@@ -23,19 +25,19 @@ app.get('/webhook', (req, res) => {
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-    
+
     // Checks if a token and mode is in the query string of the request
-    if (mode && token) {        
+    if (mode && token) {
         // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === config.verifyToken) {            
+        if (mode === 'subscribe' && token === config.verifyToken) {
             // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge)
         } else {
             // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403); 
+            res.sendStatus(403);
         }
-    }    
+    }
 });
 
 // Creates the endpoint for your webhook
@@ -46,9 +48,9 @@ app.post('/webhook', (req, res) => {
     if (body.object === 'page') {
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
-        
+
         // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {            
+        body.entry.forEach(function(entry) {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0            
             let webhookEvent = entry.messaging[0];
@@ -58,7 +60,7 @@ app.post('/webhook', (req, res) => {
             let senderPsid = webhookEvent.sender.id;
 
             // Si el usuario no esta en users, crea uno nuevo
-            if(!(senderPsid in users)) {
+            if (!(senderPsid in users)) {
                 let user = new User(senderPsid);
                 console.log('Psid:', senderPsid);
 
@@ -78,7 +80,7 @@ app.post('/webhook', (req, res) => {
                         receivedMessage.handleMessage();
                     });
             } else {
-                i18n.setLocale(users[senderPsid]. locale);
+                i18n.setLocale(users[senderPsid].locale);
                 console.log('El perfil ya existe, PSID:', senderPsid, 'con locale:', i18n.getLocale());
                 let dato;
                 let receivedMessage = new Receive(users[senderPsid], webhookEvent);
@@ -87,25 +89,43 @@ app.post('/webhook', (req, res) => {
                 if (dato === 'Emprendimiento Digital' || dato === 'Transformación Digital' || dato === 'Diseño Web') {
                     users[senderPsid]['servicio'] = dato;
                 } else if (dato === 'Idea' || dato === 'Modelo de Negocio' || dato === 'Capital semilla y/o primeras ventas' ||
-                           dato === 'No tengo sistema' || dato === 'Mi sistema es insuficiente' || dato === 'Mi sistema es costoso' ||
-                           dato === 'Landing Page' || dato === 'Tienda en Línea' || dato === 'Aplicación Web') {
+                    dato === 'No tengo sistema' || dato === 'Mi sistema es insuficiente' || dato === 'Mi sistema es costoso' ||
+                    dato === 'Landing Page' || dato === 'Tienda en Línea' || dato === 'Aplicación Web') {
                     users[senderPsid]['proyecto'] = dato;
                 } else if (dato.includes('@') || dato === 'MAS_TARDE') {
                     users[senderPsid]['email'] = dato;
                 } else if (dato.includes('+') || Number(dato) || dato === 'MAS_TARDE2') {
                     users[senderPsid]['tel'] = dato;
+
+                    // Conexión a la base de datos de mongo e inserción del documento
+                    try {
+                        mongoClient.connect(urlDB, (err, db) => {
+                            if (err) throw err;
+                            let dbo = db.db('yoinnMx');
+                            let myDoc = users[senderPsid];
+
+                            dbo.collection('messengerLeads').insertOne(myDoc, (err, res) => {
+                                if (err) throw err;
+                                console.log('Se insertó un documento');
+                                db.close();
+                            })
+                        })
+                    } catch (err) {
+                        console.dir(err);
+                    }
+
                 } else if (typeof dato === 'string') {
                     users[senderPsid]['nombreProyecto'] = dato;
                 }
                 console.log(users);
-            }            
-        });                
+            }
+        });
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
 });
-    
+
 
 //Set up your App's Messenger Profile
 app.get('/profile', (req, res) => {
@@ -144,7 +164,7 @@ app.get('/profile', (req, res) => {
 // Verify that the callback came from Facebook.
 function verifyRequestSignature(req, res, buf) {
     var signature = req.headers["x-hub-signature"];
-  
+
     if (!signature) {
         console.log("Couldn't validate the signature.");
     } else {
@@ -161,5 +181,5 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 config.checkEnvVariables();
-    
+
 module.exports = app;
